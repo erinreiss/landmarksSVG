7dca0bde6de29f18145a6f251886bba3b6049dc5
erinreiss <reiss.aaron@gmail.com>
2020-10-20 11:08:44 -0500

    corrected the 8 sites

--- js/script.js
+++ js/script.js
@@ -18,7 +18,6 @@ $(document).ready(function(){
 	$('#51-Bayard').attr("lSpeakerName","Margaret Yuen")
 });
 
-
 // Highlight location with mouse over
 
 $(locations).mouseenter(
@@ -37,29 +36,25 @@ $(locations).mouseleave(
 	}
 );
 
+// Location Click Listener
 
-//Next2 Element Sound Click Listeners
-	// var audiof = $("#fPop")[0];
-	// $('#forsythPop').click(function() {
-	//   	audiof.pause();
- //    	audiof.load();
- //    	audiof.play();
-	// });
-
-// Streets Click Listener
 $(locations).click(
 	function(){
 		// $('#clickStreet').css('visibility','hidden');
 		// $('#locationPopup').css('visibility','visible');
 		var thisAudio = ("audio/"+this.id+".mp3");
 		var thisPhoto = ("images/"+this.id+"-a.jpg");
+		var thisSpeaker = (this.data-speaker);
+		console.log(this);
 		console.log(this.id);
 		console.log(this.class);
 		console.log(this.data-speaker);
+		console.log(this.data);
+		console.log(this.thisSpeaker);
 		changeTrack(thisAudio);
 		$('#speakerPhoto').attr("src",thisPhoto);
 		$('.lSpeaker').text(this.title);
-
+		//old stuff im keeping for now
 				// console.log(matchData[i].lAudio);
 				// $('.lSpeaker').text(matchData[i].lSpeaker);
 				// $('.jobFill').text(matchData[i].jobFill);
@@ -82,76 +77,3 @@ function changeTrack(sourceUrl) {
     audioDues[0].load();//suspends and restores all audio element
     audioDues[0].play();
 }
-
-// // Next3 Element Click Listeners, why are you so lazy?
-
-// $('#english').click(
-// 	function(){
-// 		$('#intro3eng').css('visibility','visible');
-// 		$('#intro3man').css('visibility','hidden');
-// 		$('#intro3fuj').css('visibility','hidden');
-// 		$('#intro3can').css('visibility','hidden');
-// 	}
-// );
-// $('#mandarin').click(
-// 	function(){
-// 		$('#intro3eng').css('visibility','hidden');
-// 		$('#intro3man').css('visibility','visible');
-// 		$('#intro3fuj').css('visibility','hidden');
-// 		$('#intro3can').css('visibility','hidden');
-// 	}
-// );
-// $('#fujianese').click(
-// 	function(){
-// 		$('#intro3eng').css('visibility','hidden');
-// 		$('#intro3man').css('visibility','hidden');
-// 		$('#intro3fuj').css('visibility','visible');
-// 		$('#intro3can').css('visibility','hidden');
-// 	}
-// );
-// $('#cantonese').click(
-// 	function(){
-// 		$('#intro3eng').css('visibility','hidden');
-// 		$('#intro3man').css('visibility','hidden');
-// 		$('#intro3fuj').css('visibility','hidden');
-// 		$('#intro3can').css('visibility','visible');
-// 	}
-// );
-
-// 	//Next3 Element Sound Click Listeners
-// 	var audio1 = $("#mbE")[0];
-// 	$('#english').click(function() {
-// 	  	audio1.pause();
-//     	audio1.load();
-//     	audio1.play();
-// 	});
-// 	var audio2 = $("#mbF")[0];
-// 	$('#fujianese').click(function() {
-// 	  	audio2.pause();
-//     	audio2.load();
-//     	audio2.play();
-// 	});
-// 	var audio3 = $("#mbM")[0];
-// 	$('#mandarin').click(function() {
-// 	  	audio3.pause();
-//     	audio3.load();
-//     	audio3.play();
-// 	});
-// 	var audio4 = $("#mbC")[0];
-// 	$('#cantonese').click(function() {
-// 	  	audio4.pause();
-//     	audio4.load();
-//     	audio4.play();
-// 	});
-
-// // Skip Intro Click Listener
-
-// $('#skipIntro').click(
-// 	function(){
-// 		$('#skipIntro').css('visibility','hidden');
-// 		$('.introPop').css('display','none');
-// 		$(hLights).css('visibility','visible');
-// 		$('#clickStreet').css('visibility','visible');
-// 	}
-// );
-
